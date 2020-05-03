125ec0c73e5657b604389bcf4914996c9681483f
deviayulestari <55142624+deviayulestari@users.noreply.github.com>
2020-05-03 16:19:32 -0700

    mengedit file

--- kuliah/pertemuan12/login.php
+++ kuliah/pertemuan12/login.php
@@ -21,36 +21,79 @@ if (isset($_POST['login'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- my css -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <!-- my fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Kaushan+Script&display=swap" rel="stylesheet">
+
   <title>Login</title>
+  <style>
+    /* halaman login.php */
+    .container-login {
+      height: 215px;
+      width: 350px;
+      background-color: lavender;
+      color: black;
+      padding: 20px;
+      margin: 50px auto;
+      border-radius: 35px;
+    }
+
+    .container-login h3 {
+      text-align: center;
+      font-size: 40px;
+      margin: 15px auto;
+      font-family: Shrikhand;
+    }
+
+    .container-login label {
+      padding-right: 40px;
+      font-size: 18px;
+      font-family: Libre Baskerville;
+    }
+
+    .container-login .tombol {
+      margin: 15px auto;
+      text-align: center;
+    }
+
+    .container-login .registrasi {
+      font-size: 15px;
+      font-family: Libre Baskerville;
+      text-align: center;
+    }
+  </style>
 </head>
 
 <body>
-  <h3>Form Login</h3>
-  <?php if (isset($login['error'])) : ?>
-    <p style="color: red; font-style:italic;"><?= $login['pesan']; ?></p>
-  <?php endif; ?>
-  <form action="" method="POST">
-    <ul>
-      <li>
-        <label>
-          Username :
-          <input type="text" name="username" autofocus autocomplete="off" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Password :
-          <input type="password" name="password" required>
-        </label>
-      </li>
-      <li>
-        <button type="submit" name="login">Login!</button>
-      </li>
-      <li>
+  <div class="container-login">
+    <h3>Form Login</h3>
+    <?php if (isset($login['error'])) : ?>
+      <p style="color: red; font-style:italic;"><?= $login['pesan']; ?></p>
+    <?php endif; ?>
+    <form action="" method="POST">
+      <table>
+        <tr>
+          <td><label for="username">Username</label></td>
+          <td>:</td>
+          <td><input type="text" name="username"></td>
+        </tr>
+        <tr>
+          <td><label for="password">Password</label></td>
+          <td>:</td>
+          <td><input type="password" name="password"></td>
+        </tr>
+      </table>
+      <div class="tombol">
+        <button type="submit" name="login">LOGIN</button>
+      </div>
+      <div class="registrasi">
         <a href="registrasi.php">Tambah User Baru</a>
-      </li>
-    </ul>
-  </form>
+      </div>
+    </form>
+  </div>
 </body>
 
 </html>
--- kuliah/pertemuan12/registrasi.php
+++ kuliah/pertemuan12/registrasi.php
@@ -19,36 +19,89 @@ if (isset($_POST['registrasi'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- my css -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <!-- my fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Kaushan+Script&display=swap" rel="stylesheet">
+
   <title>Registrasi</title>
+  <style>
+    /* halaman login.php */
+    .container-registrasi {
+      height: 200px;
+      width: 400px;
+      background-color: lavender;
+      color: black;
+      padding: 20px;
+      margin: 50px auto;
+      border-radius: 35px;
+    }
+
+    .container-registrasi h3 {
+      text-align: center;
+      font-size: 40px;
+      margin: 15px auto;
+      font-family: Shrikhand;
+    }
+
+    .container-registrasi label {
+      font-size: 18px;
+      font-family: Libre Baskerville;
+    }
+
+    .container-registrasi .tombol {
+      margin: 15px auto;
+      text-align: center;
+    }
+  </style>
 </head>
 
 <body>
-  <h3>Form Registrasi</h3>
-  <form action="" method="POST">
-    <ul>
-      <li>
-        <label>
-          Username :
-          <input type="text" name="username" autofocus autocomplete="off" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Password :
-          <input type="password" name="password1" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Konfirmasi Password :
-          <input type="password" name="password2" required>
-        </label>
-      </li>
-      <li>
+  <div class="container-registrasi">
+    <h3>Form Registrasi</h3>
+    <form action="" method="POST">
+      <table>
+        <tr>
+          <td>
+            <label>
+              Username
+          </td>
+          <td>:</td>
+          <td>
+            <input type="text" name="username" autofocus autocomplete="off" required>
+            </label>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>
+              Password
+          </td>
+          <td>:</td>
+          <td>
+            <input type="password" name="password1" required>
+            </label>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label>
+              Konfirmasi Password
+          </td>
+          <td>:</td>
+          <td>
+            <input type="password" name="password2" required>
+            </label>
+          </td>
+        </tr>
+      </table>
+      <div class="tombol">
         <button type="submit" name="registrasi">Registrasi!</button>
-      </li>
-    </ul>
-  </form>
+      </div>
+    </form>
+  </div>
 </body>
 
 </html>
