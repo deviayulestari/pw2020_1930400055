a17d27c97f32c272b6f2016669b10b4843760dd7
deviayulestari <55142624+deviayulestari@users.noreply.github.com>
2020-05-07 21:42:28 -0700

    mengedit file

--- kuliah/pertemuan13/index.php
+++ kuliah/pertemuan13/index.php
@@ -49,44 +49,49 @@ if (isset($_POST['cari'])) {
       <div class="col s3"></div>
       <div class="col s6  blue-grey darken-1">
         <form action="" method="POST">
-          <input type="text" name="keyword" autofocus placeholder="Masukkan keyword pencarian..." autocomplete="off">
+          <input type="text" name="keyword" autofocus placeholder="Masukkan keyword pencarian..." autocomplete="off" class="keyword">
       </div>
       <div class="col s1">
-        <button class="btn   blue-grey darken-1" type="submit" name="cari">Cari!</button>
+        <button class="tombol-cari btn blue-grey darken-1" type="submit" name="cari">Cari!</button>
       </div>
       </form>
       <div class="row"></div>
     </div>
 
-    <table border="1" cellpadding="10" cellspacing="0">
-      <tr>
-        <th>#</th>
-        <th>Gambar</th>
-        <th>Nama</th>
-        <th>Aksi</th>
-      </tr>
-
-      <?php if (empty($mahasiswa)) : ?>
+    <div class="container">
+      <table border="1" cellpadding="10" cellspacing="0">
         <tr>
-          <td colspan="4">
-            <p style="color: red; font-style: italic;">Data Mahasiswa Tidak Ditemukan!</p>
-          </td>
+          <th>#</th>
+          <th>Gambar</th>
+          <th>Nama</th>
+          <th>Aksi</th>
         </tr>
-      <?php endif; ?>
 
-      <?php $i = 1;
-      foreach ($mahasiswa as $m) : ?>
-        <tr>
-          <td><?= $i++; ?></td>
-          <td><img src="img/<?= $m['gambar']; ?>" width="150"></td>
-          <td><?= $m['nama']; ?></td>
-          <td>
-            <button class="btn waves-effect grey lighten-2"><a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a></button>
-          </td>
-        <?php endforeach; ?>
-        </tr>
-    </table>
+        <?php if (empty($mahasiswa)) : ?>
+          <tr>
+            <td colspan="4">
+              <p style="color: red; font-style: italic;">Data Mahasiswa Tidak Ditemukan!</p>
+            </td>
+          </tr>
+        <?php endif; ?>
+
+        <?php $i = 1;
+        foreach ($mahasiswa as $m) : ?>
+          <tr>
+            <td><?= $i++; ?></td>
+            <td><img src="img/<?= $m['gambar']; ?>" width="150"></td>
+            <td><?= $m['nama']; ?></td>
+            <td>
+              <button class="btn waves-effect grey lighten-2"><a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a></button>
+            </td>
+          <?php endforeach; ?>
+          </tr>
+      </table>
+    </div>
   </div>
+
+  <script src="js/script.js"></script>
+
 </body>
 
 </html>
