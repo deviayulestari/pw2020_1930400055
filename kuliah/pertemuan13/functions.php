76e52d2d4eeeba3db3b6edc386f277937eba7719
deviayulestari <55142624+deviayulestari@users.noreply.github.com>
2020-05-12 09:46:46 -0700

    menambahkan upload file

--- kuliah/pertemuan13/functions.php
+++ kuliah/pertemuan13/functions.php
@@ -34,10 +34,10 @@ function upload()
 
   //ketika tidak ada gambar yang dipilih
   if ($error == 4) {
-    echo "<script> 
-          alert('pilih gambar terlebih dahulu!');
-          </script>";
-    return false;
+    // echo "<script> 
+    //       alert('pilih gambar terlebih dahulu!');
+    //       </script>";
+    return 'nophoto.jpg';
   }
 
   //cek ekstensi file
@@ -107,6 +107,13 @@ function tambah($data)
 function hapus($id)
 {
   $conn = koneksi();
+
+  //menghapus gambar di folder img
+  $mhs = query("SELECT * FROM mahasiswa WHERE id = $id");
+  if ($mhs['gambar'] != 'nophoto.jpg') {
+    unlink('img/' . $mhs['gambar']);
+  }
+
   mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id") or die(mysqli_error($conn));
   return mysqli_affected_rows($conn);
 }
@@ -120,7 +127,16 @@ function ubah($data)
   $nrp = htmlspecialchars($data['nrp']);
   $email = htmlspecialchars($data['email']);
   $jurusan = htmlspecialchars($data['jurusan']);
-  $gambar = htmlspecialchars($data['gambar']);
+  $gambar_lama = htmlspecialchars($data['gambar_lama']);
+
+  $gambar = upload();
+  if (!$gambar) {
+    return false;
+  }
+
+  if ($gambar == 'nophoto.jpg') {
+    $gambar = $gambar_lama;
+  }
 
   $query = "UPDATE mahasiswa SET
                 nama = '$nama',
